6ceff6fad58fd15aa114c4fdd2cc4e4044dd1be5
cncoksoyler <cncoksoyler@gmail.com>
2021-11-29 16:51:52 +0300

    MachineSeeder okay

--- database/factories/MachineFactory.php
+++ database/factories/MachineFactory.php
@@ -3,6 +3,9 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
 
 class MachineFactory extends Factory
 {
@@ -13,8 +16,17 @@ class MachineFactory extends Factory
      */
     public function definition()
     {
+
+       $areaEnum = ['stamping','assembly','overmoulding','plating','heat treatement','fineblanking','others'];
+       $amgEnum = ['AMG7','AMG8'];
         return [
-            //
+            
+            'name' => $this->faker->name(),
+            'AMG' => $amgEnum[rand(0,1)],
+            'area' => $areaEnum[rand(0,6)], // password
+            
         ];
+        
+      
     }
 }
--- database/seeders/MachineSeeder.php
+++ database/seeders/MachineSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Machine;
+use Faker\Factory;
 use Illuminate\Database\Seeder;
 
 class MachineSeeder extends Seeder
@@ -13,6 +15,21 @@ class MachineSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Factory::create();
+        $areaEnum = ['stamping','assembly','overmoulding','plating','heat treatement','fineblanking','others'];
+        $amgEnum = ['AMG7','AMG8'];
+        for ($i = 0;$i<10; $i++) {
+            Machine::create([
+                'name' => $faker->name,
+                'AMG' => $amgEnum[rand(0,1)],
+                'area' => $areaEnum[rand(0,6)], // password
+            ]);
+        };
+
+        
+        
+            
+             
+         
     }
 }
