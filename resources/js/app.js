8f027425a3ffdcf291bd36d7451b768dd2208af1
Baris Menderes <bmenderes@gmail.com>
2021-12-08 16:36:06 +0300

    vue eklendi

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,13 @@
 require('./bootstrap');
 
+import { createApp } from 'vue';
+
+import App from './components/App.vue';
+
+createApp(App).mount("#app")
+
 import Alpine from 'alpinejs';
+
 
 window.Alpine = Alpine;
 
---
+++ resources/js/components/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <h2>Hi from Vue 3 </h2>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
