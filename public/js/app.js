942ef20ea1eb81e3d8200a47ab55c5472b41443e
Baris Menderes <bmenderes@gmail.com>
2021-12-13 15:53:19 +0300

    bootstrap installed

--- public/js/app.js
+++ public/js/app.js
@@ -24382,7 +24382,7 @@ __webpack_require__.r(__webpack_exports__);
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
   setup: function setup() {
-    var name = (0,vue__WEBPACK_IMPORTED_MODULE_0__.ref)('baris');
+    var name = (0,vue__WEBPACK_IMPORTED_MODULE_0__.ref)("baris");
     return {
       name: name
     };
